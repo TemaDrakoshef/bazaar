90dbb7f095db8ab2c02729c6b208b2bddf14381a
TemaDrakoshef <artem125149@gmail.com>
2026-08-15 00:57:59 +0300

    feat(catalog): register all CRUD use cases in DI container

--- services/catalog_service/src/infrastructure/di/container.py
+++ services/catalog_service/src/infrastructure/di/container.py
@@ -10,6 +10,18 @@ from sqlalchemy.ext.asyncio import (
 
 from src.application.use_cases.create_category import CreateCategoryUseCase
 from src.application.use_cases.create_product import CreateProductUseCase
+from src.application.use_cases.delete_category import DeleteCategoryUseCase
+from src.application.use_cases.delete_product import DeleteProductUseCase
+from src.application.use_cases.read_category import (
+    ReadCategoryUseCase,
+)
+from src.application.use_cases.read_list_category import (
+    ReadListCategoriesUseCase,
+)
+from src.application.use_cases.read_list_products import ReadListProductsUseCase
+from src.application.use_cases.read_product import ReadProductUseCase
+from src.application.use_cases.update_category import UpdateCategoryUseCase
+from src.application.use_cases.update_product import UpdateProductUseCase
 from src.domain.interfaces.unit_of_work import AbstractUnitOfWork
 from src.infrastructure.config.settings import Settings
 from src.infrastructure.database.unit_of_work import SQLAlchemyUnitOfWork
@@ -52,3 +64,39 @@ class CatalogProvider(Provider):
     @provide(scope=Scope.REQUEST)
     def provide_create_category(self, uow: AbstractUnitOfWork) -> CreateCategoryUseCase:
         return CreateCategoryUseCase(uow)
+
+    @provide(scope=Scope.REQUEST)
+    def provide_read_category(self, uow: AbstractUnitOfWork) -> ReadCategoryUseCase:
+        return ReadCategoryUseCase(uow)
+
+    @provide(scope=Scope.REQUEST)
+    def provide_read_list_categories(
+        self, uow: AbstractUnitOfWork
+    ) -> ReadListCategoriesUseCase:
+        return ReadListCategoriesUseCase(uow)
+
+    @provide(scope=Scope.REQUEST)
+    def provide_read_product(self, uow: AbstractUnitOfWork) -> ReadProductUseCase:
+        return ReadProductUseCase(uow)
+
+    @provide(scope=Scope.REQUEST)
+    def provide_read_list_products(
+        self, uow: AbstractUnitOfWork
+    ) -> ReadListProductsUseCase:
+        return ReadListProductsUseCase(uow)
+
+    @provide(scope=Scope.REQUEST)
+    def provide_update_product(self, uow: AbstractUnitOfWork) -> UpdateProductUseCase:
+        return UpdateProductUseCase(uow)
+
+    @provide(scope=Scope.REQUEST)
+    def provide_delete_product(self, uow: AbstractUnitOfWork) -> DeleteProductUseCase:
+        return DeleteProductUseCase(uow)
+
+    @provide(scope=Scope.REQUEST)
+    def provide_update_category(self, uow: AbstractUnitOfWork) -> UpdateCategoryUseCase:
+        return UpdateCategoryUseCase(uow)
+
+    @provide(scope=Scope.REQUEST)
+    def provide_delete_category(self, uow: AbstractUnitOfWork) -> DeleteCategoryUseCase:
+        return DeleteCategoryUseCase(uow)
